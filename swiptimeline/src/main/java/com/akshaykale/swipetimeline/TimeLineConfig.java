72f389e2080056fd7b69a8edf28953f7e7f25549
akshay.kale <akshay.kale@rakuten.com>
2017-08-22 14:48:38 +0900

    Timeline indicator added. Sort by groups added

--- swiptimeline/src/main/java/com/akshaykale/swipetimeline/TimeLineConfig.java
+++ swiptimeline/src/main/java/com/akshaykale/swipetimeline/TimeLineConfig.java
@@ -12,48 +12,56 @@ import java.util.HashMap;
 
 public class TimeLineConfig {
 
-    public static HashMap<String, ArrayList<TimeLineObject>> timelineObjMap = new HashMap<>();
+    public static HashMap<String, ArrayList<TimelineObject>> timelineObjMap = new HashMap<>();
     public static ArrayList<String> headerList = new ArrayList<>();
 
-
-    public static void setData(ArrayList<TimeLineObject> list, String sortby){
+    public static void setData(ArrayList<TimelineObject> list, TimelineGroupType type) {
 
         ArrayList<String> tabsList = new ArrayList<>();
-        ArrayList<TimeLineObject> _list = null;
-        HashMap<String, ArrayList<TimeLineObject>> map = new HashMap<>();
-        switch (sortby) {
-            case "date":
-                break;
-            case "month":
-                for (int i = 0; i < list.size(); i++) {
-                    TimeLineObject photo = list.get(i);
-                    long ts = photo.getTimestamp();
-                    Date date = new Date(ts);
-                    String month = (String) DateFormat.format("MMM", date);
-                    String year = (String) DateFormat.format("yyyy", date);
-                    String tabTitle = month + ", " + year;
+        ArrayList<TimelineObject> _list = null;
+        HashMap<String, ArrayList<TimelineObject>> map = new HashMap<>();
 
-                    if (tabsList.contains(tabTitle)) {
-                        _list.add(photo);
-                        if (i == list.size() - 1) {
-                            map.put(tabTitle, _list);
-                        }
-                    } else {
-                        if (_list != null) {
-                            map.put(tabsList.get(tabsList.size() - 1), _list);
-                        }
-                        _list = new ArrayList<>();
-                        _list.clear();
-                        tabsList.add(tabTitle);
-                        _list.add(photo);
-                        if (i == list.size() - 1) {
-                            map.put(tabTitle, _list);
-                        }
-                    }
+        for (int i = 0; i < list.size(); i++) {
+            TimelineObject photo = list.get(i);
+            long ts = photo.getTimestamp();
+            Date date = new Date(ts);
+            String day = (String) DateFormat.format("dd", date);
+            String month = (String) DateFormat.format("MMM", date);
+            String year = (String) DateFormat.format("yyyy", date);
+            String tabTitle = "";
+            switch (type) {
+                case DAY:
+                    tabTitle = day + " " + month + ", " + year;
+                    break;
+                case MONTH:
+                    tabTitle = month + ", " + year;
+                    break;
+                case YEAR:
+                    tabTitle = year;
+            }
+            if (tabsList.contains(tabTitle)) {
+                _list.add(photo);
+                if (i == list.size() - 1) {
+                    ArrayList<TimelineObject> obj = map.get(tabTitle);
+                    if (obj != null && obj.size() > 0) {
+
+                        obj.addAll(_list);
+                        map.put(tabTitle, obj);
+                    } else
+                        map.put(tabTitle, _list);
                 }
-                break;
-            case "year":
-                break;
+            } else {
+                if (_list != null) {
+                    map.put(tabsList.get(tabsList.size() - 1), _list);
+                }
+                _list = new ArrayList<>();
+                _list.clear();
+                tabsList.add(tabTitle);
+                _list.add(photo);
+                if (i == list.size() - 1) {
+                    map.put(tabTitle, _list);
+                }
+            }
         }
 
         timelineObjMap = map;
---
+++ swiptimeline/src/main/java/com/akshaykale/swipetimeline/TimelineGroupType.java
@@ -0,0 +1,12 @@
+package com.akshaykale.swipetimeline;
+
+/**
+ * Created by akshay.kale on 22/08/2017.
+ */
+
+public enum TimelineGroupType {
+
+    DAY,
+    MONTH,
+    YEAR
+}
